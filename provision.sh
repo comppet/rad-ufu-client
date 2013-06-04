6ed57d3e966325067eaae32c16398299df44c63d
Yassin <yassin791@gmail.com>
2013-06-04 17:39:19 -0300

    one time provision

--- provision.sh
+++ provision.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# testa se o script já foi executado e termina a execução caso se verdadeiro
+test -f /etc/bootstraped && exit
+
 # Proxy da UFU :/
-# export http_proxy=http://proxy.ufu.br:3128/
+PROXY_UFU="http://proxy.ufu.br:3128/"
+export http_proxy=$PROXY_UFU
 
 apt-get update
 
@@ -50,3 +54,6 @@ cd /vagrant && npm install
 
 # configura o projeto
 grunt setup
+
+# Sinaliza que este script já foi executado
+date > /etc/bootstraped
